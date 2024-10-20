e9ed8d040e1b8cd923d8063c9ff1c50af4d2e5eb
Michael <michaelvargo@outlook.com>
2024-10-20 14:17:27 -0500

    Triangular Numbers

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,55 @@
 package com.csc;
 
+import java.util.Scanner;
+
 public class TriangleNumberCalculator {
-  // Your code goes here!
+  int subcount;
+  int addcount;
+  int flag = 0;
+  int copy;
+  int negative;
+
+  public int value(int n) {
+    int m = n;
+    return add(n, m);
+  }
+
+  public int add(int n, int m) {
+    if (m == 0)
+      return n;
+    n = n + addcount;
+    addcount++;
+    return add(n, m - 1);
+  }
+
+  public int sub(int n, int m) {
+    if (flag == 0) {
+      copy = add(n, n);
+      subcount = m;
+      flag += 1;
+      if (n < m)
+        negative += 1;
+      n = 0;
+    }
+    if (negative == 1) {
+      if (m < 0)
+        return n;
+      n = copy - subcount;
+      subcount++;
+      return sub(n, m - 1);
+    } else {
+      if (m == 0)
+        return n;
+      n = copy - subcount;
+      subcount++;
+      return sub(n, m - 1);
+    }
+  }
+
+  public static void main(String[] args) {
+    TriangleNumberCalculator number = new TriangleNumberCalculator();
+    try (Scanner input = new Scanner(System.in)) {
+    }
+    System.out.print(number.sub(2, 3));
+  }
 }
--- src/test/java/com/csc/TestTriangleNumberCalculator.java
+++ src/test/java/com/csc/TestTriangleNumberCalculator.java
@@ -1,15 +1,34 @@
 package com.csc;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class TestTriangleNumberCalculator {
-
-  TriangleNumberCalculator calculator;
+  TriangleNumberCalculator Calculator;
 
   @BeforeEach
-  void setUp() {
-    calculator = new TriangleNumberCalculator();
+  public void setup() {
+    Calculator = new TriangleNumberCalculator();
   }
 
-  // Your tests go here
+  @Test
+  public void triangletest() {
+    assertEquals(3, Calculator.add(2, 2));
+  }
+
+  @Test
+  public void subtriangletest() {
+    assertEquals(-3, Calculator.sub(2, 3));
+  }
+
+  @Test
+  public void subpartial() {
+    assertEquals(0, Calculator.sub(1, 1));
+  }
+  @Test
+  public void valuetest()
+  {
+    assertEquals(3, Calculator.value(2));
+  }
 }
